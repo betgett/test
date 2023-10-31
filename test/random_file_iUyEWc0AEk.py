1b55a3ac94b51aa061d7a15cffe8ae143080e7ff
betegtt <wangyan542020@outlook.com>
2023-10-31 22:59:55 +0800

    Add random file

--- test/random_file_iUyEWc0AEk.py
+++
@@ -1,8 +0,0 @@
-ForEach-Object : 无法绑定参数“RemainingScripts”。无法将“System.String”类型的“*”值转换为“System.Management.Automa
-tion.ScriptBlock”类型。
-所在位置 行:1 字符: 40
-+ ... inimum 0 -Maximum 256) | ForEach-Object { [char]([byte]$_) } * 390625
-+                              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    + CategoryInfo          : InvalidArgument: (:) [ForEach-Object]，ParameterBindingException
-    + FullyQualifiedErrorId : CannotConvertArgumentNoMessage,Microsoft.PowerShell.Commands.ForEachObjectCommand
- 
